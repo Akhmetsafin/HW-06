18156e3a6fe736602f7dc6677b01b72c416a182c
Artur <artur071982@gmail.com>
2021-03-13 14:50:16 +0500

    работа над ошибками

--- Process/Program.cs
+++ Process/Program.cs
@@ -18,51 +18,93 @@ namespace Process1
                 var letgh_ID = Convert.ToString(procesS.Id);
                 Console.WriteLine($"ID: {procesS.Id}{new string(' ', 10 - letgh_ID.Length)} Name: {procesS.ProcessName}");
             }
-            var i = 100;
+
             Console.WriteLine("Введиде способ завершения процесса: 1- по ID, 2 - по имени");
             //int varKill = Convert.ToInt32(Console.ReadLine());
 
 
+
+
+            var s = Console.ReadLine();
+            if (s == "1")
             {
-                string s = Console.ReadLine();
+                Console.WriteLine("Введиде ID");
+                using Process Pid = Process.GetProcessById(Int32.Parse(Console.ReadLine()));
+                Pid.Kill();
+                //.WaitForExit();
+                Console.WriteLine($"killed {Pid}");
 
-                while (i > 0)
-                {
-                    if (string.IsNullOrEmpty(s))
-                        break;
-                    if (s == "1")
-                    {
+            }
+
+
+
+
+
+        }
+    }
+}          /*
+                    case "2":
+
                         try
                         {
-                            Console.WriteLine("Введиде ID:"); 
-                           Process.GetProcessById(Int32.Parse(Console.ReadLine())).Kill();
-                            Process.GetProcessById(Int32.Parse(Console.ReadLine())).WaitForExit();
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine("no correct.");
-                            continue;
-                        }
-
-                    }
-                    if (s == "2")
-                    {
-                        try
-                        {
-                            Console.WriteLine("Введиде имя процесса:");
+                            Console.WriteLine("Введиде имя процесса");
                             Process.GetProcessesByName(Console.ReadLine())[0].Kill();
                             Process.GetProcessesByName(Console.ReadLine())[0].WaitForExit();
                         }
                         catch (Exception e)
                         {
                             Console.WriteLine("no correct.");
-                            continue;
+
                         }
-                    }
-                    
+                        break;
+
+                    default:
+                        Console.WriteLine("Неправильно выбран режим");
+                        break;
                 }
+            }
+            */
+
+
+/*
+
+{
+    string s = Console.ReadLine();
+
+    while (i > 0)
+    {
+        if (string.IsNullOrEmpty(s))
+            break;
+        if (s == "1")
+        {
+            try
+            {
+                Console.WriteLine("Введиде ID"); 
+               Process.GetProcessById(Int32.Parse(Console.ReadLine())).Kill();
+                Process.GetProcessById(Int32.Parse(Console.ReadLine())).WaitForExit();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("no correct.");
+                continue;
             }
 
         }
+        if (s == "2")
+        {
+            try
+            {
+                Console.WriteLine("Введиде имя процесса");
+                Process.GetProcessesByName(Console.ReadLine())[0].Kill();
+                Process.GetProcessesByName(Console.ReadLine())[0].WaitForExit();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("no correct.");
+                continue;
+            }
+        }
+
     }
-}
+}*/
+
